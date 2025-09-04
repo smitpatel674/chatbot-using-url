9888a3b810d74365c73dec8afe22cf2045830fbc
smitpatel674 <smitp5553@gmail.com>
2025-09-04 18:30:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from ui import build_ui
 
-app = build_ui()  # 👈 expose this for gunicorn
-
 if __name__ == "__main__":
-    app.launch(server_name="0.0.0.0", server_port=8080)
+    app = build_ui()
+    app.launch(server_name="0.0.0.0", server_port=10000)
