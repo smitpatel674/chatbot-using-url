57b623d3fe908511ca44a5cf6e8c5edb3a314bbf
smitpatel674 <smitp5553@gmail.com>
2025-09-06 11:14:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
-import os
-import gradio as gr
-
-def respond(message):
-    return "You said: " + message
-
-app = gr.Blocks()
-
-with app:
-    txt = gr.Textbox(label="Enter message")
-    out = gr.Textbox(label="Response")
-    txt.submit(respond, txt, out)
+from ui import build_ui
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.launch(server_name="0.0.0.0", server_port=port)
+    demo = build_ui()
+    demo.launch(share=False, show_api=False, inbrowser=True)
