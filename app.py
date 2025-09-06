e444e2170c97619d10daed6baab83072b727ca8c
smitpatel674 <smitp5553@gmail.com>
2025-09-06 11:22:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 from ui import build_ui
 
 if __name__ == "__main__":
     demo = build_ui()
-    demo.launch(share=False, show_api=False, inbrowser=True)
+    port = int(os.environ.get("PORT", 7860))  # Render provides PORT
+    demo.launch(server_name="0.0.0.0", server_port=port, share=False, show_api=False)
